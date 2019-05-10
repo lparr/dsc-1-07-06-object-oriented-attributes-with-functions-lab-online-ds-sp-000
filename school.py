b13214878d42470707839301b777ab754b816b56
Lindsay Parr <lwcislo2@gmail.com>
2019-05-09 23:01:51 -0500

    stuck agian

--- school.py
+++ school.py
@@ -9,5 +9,8 @@ class School():
         roster[11] = []
         roster[12] = []
      
+    def roster(self):
+        return self.roster
+    
     def add_student(self, name, grade):
         self.roster[grade].append(name)
