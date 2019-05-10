2080725e01a09db32886e75b9dead12b0a2455b9
Lindsay Parr <lwcislo2@gmail.com>
2019-05-09 22:53:59 -0500

    stuck 3

--- school.py
+++ school.py
@@ -1,6 +1,13 @@
 import copy
 
 class School():
-    def __init__(self, name):
+    def __init__(self, name, roster = {}):
         self.name = name
-        self.roster = {}
+        self.roster = roster
+        roster[9] = []
+        roster[10] = []
+        roster[11] = []
+        roster[12] = []
+     
+    def add_student(self, name, grade):
+        self.roster[grade].append(name)
